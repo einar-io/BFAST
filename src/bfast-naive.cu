9f2eee323901fe5d1917290794e0dcc8339271fb
Jakob Stokholm Bertelsen <jstokh@gmail.com>
2018-10-22 23:30:00 +0200

    Move matrix transposition and scan to bfast-naive.cu

--- src/bfast-naive.cu
+++ src/bfast-naive.cu
@@ -7,6 +7,80 @@
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+// Matrix transposition
+
+__global__ void transpose_kernel(float *A, float *B, int heightA, int widthA)
+{
+
+  int gidx = blockIdx.x * blockDim.x + threadIdx.x;
+  int gidy = blockIdx.y * blockDim.y + threadIdx.y;
+
+  if(gidx >= widthA || gidy >= heightA) {
+    return;
+  }
+
+  B[IDX_2D(gidx, gidy, heightA)] = A[IDX_2D(gidy, gidx, widthA)];
+}
+
+void transpose(float *d_A, float *d_B, int heightA, int widthA)
+{
+  dim3 block(16, 16, 1);
+  dim3 grid(CEIL_DIV(widthA, block.x),
+            CEIL_DIV(heightA, block.y),
+            1);
+  transpose_kernel<<<grid, block>>>(d_A, d_B, heightA, widthA);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// For bfast_5, bfast_6, bfast_7a
+
+template <class T>
+__device__ inline T scaninc_warp_add(volatile T *in)
+{
+  const unsigned int idx  = threadIdx.x;
+  const unsigned int lane = idx & 31;
+
+  // no synchronization needed inside a WARP,
+  //   i.e., SIMD execution
+  if (lane >= 1)  { in[idx] = in[idx-1]  + in[idx]; }
+  if (lane >= 2)  { in[idx] = in[idx-2]  + in[idx]; }
+  if (lane >= 4)  { in[idx] = in[idx-4]  + in[idx]; }
+  if (lane >= 8)  { in[idx] = in[idx-8]  + in[idx]; }
+  if (lane >= 16) { in[idx] = in[idx-16] + in[idx]; }
+
+  return in[idx];
+}
+
+template <class T>
+__device__ inline void scaninc_block_add(volatile T *in)
+{
+  const unsigned int idx    = threadIdx.x;
+  const unsigned int lane   = idx &  31;
+  const unsigned int warpid = idx >> 5;
+
+  T val = scaninc_warp_add(in);
+  __syncthreads();
+
+  if (lane == 31) { in[warpid] = val; }
+  __syncthreads();
+
+  if (warpid == 0) scaninc_warp_add(in);
+  __syncthreads();
+
+  if (warpid > 0) {
+    val = in[warpid-1] + val;
+  }
+
+  __syncthreads();
+  in[idx] = val;
+  __syncthreads();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 //  Step 1: Generating X
 //
 // Output:
