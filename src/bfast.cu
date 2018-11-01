cd6059deb79f085f811e0be1696a52fd1e160890
Jakob Stokholm Bertelsen <jstokh@gmail.com>
2018-11-01 05:42:09 +0100

    Remove unnecessary untranspose in experimental BFAST that caused it not to validate. Original optimized BFAST is now back to being named "bfast-opt", whereas the experimental BFAST is now "bfast-opt-alt"

--- src/bfast.cu
+++ src/bfast.cu
@@ -63,9 +63,10 @@ extern "C" void bfast_opt(struct bfast_run_config *cfg)
   const struct bfast_step steps[] = {
     BFAST_STEP(bfast_step_1_run),
     BFAST_TRANSPOSE(X, transpose),
-    BFAST_STEP(bfast_step_2_shr_run),
+    BFAST_TRANSPOSE(Y, transpose),
+    BFAST_STEP(bfast_step_2_tiled_run),
     BFAST_STEP(bfast_step_3_run),
-    BFAST_STEP(bfast_step_4a_flipped_run),
+    BFAST_STEP(bfast_step_4a_tiled_run),
     BFAST_UNTRANSPOSE(beta0, transpose),
     BFAST_STEP(bfast_step_4b_run),
     BFAST_STEP(bfast_step_4c_tiled_run),
@@ -83,11 +84,9 @@ extern "C" void bfast_opt_alt(struct bfast_run_config *cfg)
   const struct bfast_step steps[] = {
     BFAST_STEP(bfast_step_1_run),
     BFAST_TRANSPOSE(X, transpose),
-    BFAST_TRANSPOSE(Y, transpose),
-    BFAST_STEP(bfast_step_2_tiled_run),
+    BFAST_STEP(bfast_step_2_shr_run),
     BFAST_STEP(bfast_step_3_run),
-    BFAST_STEP(bfast_step_4a_tiled_run),
-    BFAST_UNTRANSPOSE(beta0, transpose),
+    BFAST_STEP(bfast_step_4a_flipped_run),
     BFAST_STEP(bfast_step_4b_run),
     BFAST_STEP(bfast_step_4c_tiled_run),
     BFAST_STEP(bfast_step_5_run),
@@ -96,7 +95,7 @@ extern "C" void bfast_opt_alt(struct bfast_run_config *cfg)
     BFAST_STEP(bfast_step_7b_run),
     BFAST_STEP(bfast_step_8_opt2_run)
   };
-  bfast_run(cfg, "bfast-opt", steps, NUM_ELEMS(steps));
+  bfast_run(cfg, "bfast-opt-alt", steps, NUM_ELEMS(steps));
 }
 
 
