4cbb1b9bad1e001f4334e124d3012db8bd6b4abf
Jakob Stokholm Bertelsen <jstokh@gmail.com>
2018-11-02 17:11:23 +0100

    De-cosminify transpose related functions

--- src/kernels/bfast_helpers.cu.h
+++ src/kernels/bfast_helpers.cu.h
@@ -9,13 +9,10 @@
 static __global__ void transpose_kernel_NAIVE(float *A, float *B,
                                               int heightA, int widthA)
 {
-
   int gidx = blockIdx.x * blockDim.x + threadIdx.x;
   int gidy = blockIdx.y * blockDim.y + threadIdx.y;
 
-  if(gidx >= widthA || gidy >= heightA) {
-    return;
-  }
+  if(gidx >= widthA || gidy >= heightA) { return; }
 
   B[IDX_2D(gidx, gidy, heightA)] = A[IDX_2D(gidy, gidx, widthA)];
 }
@@ -39,6 +36,9 @@ template <class ElTp, int T>
 __global__ void transpose_tiled_kernel(ElTp* A, ElTp* B,
                                        int heightA, int widthA)
 {
+  // Grid:  (CEIL_DIV(widthA, T), CEIL_DIV(heightA, T), 1)
+  // Block: (T, T, 1)
+
   extern __shared__ char sh_mem1[];
   volatile ElTp *tile = (volatile ElTp *)sh_mem1;
 
@@ -46,36 +46,30 @@ __global__ void transpose_tiled_kernel(ElTp* A, ElTp* B,
   int y = blockIdx.y * T + threadIdx.y;
 
   if (x < widthA && y < heightA) {
-    tile[threadIdx.y*(T+1) + threadIdx.x] = A[y*widthA + x];
+    tile[IDX_2D(threadIdx.y, threadIdx.x, T + 1)] = A[IDX_2D(y, x, widthA)];
   }
-
   __syncthreads();
 
   x = blockIdx.y * T + threadIdx.x;
   y = blockIdx.x * T + threadIdx.y;
-
   if (x < heightA && y < widthA) {
-    B[y*heightA + x] = tile[threadIdx.x*(T+1) + threadIdx.y];
+    B[IDX_2D(y, x, heightA)] = tile[IDX_2D(threadIdx.x, threadIdx.y, T + 1)];
   }
 }
 
 template<class ElTp, int T>
-void transpose_tiled ( ElTp*              d_in,
-                       ElTp*              d_out,
-                       const unsigned int height,
-                       const unsigned int width)
+void transpose_tiled(ElTp* d_in, ElTp* d_out,
+                     const unsigned int height,
+                     const unsigned int width)
 {
-   // 1. setup block and grid parameters
-   unsigned int sh_mem_size = T * (T+1) * sizeof(ElTp);
-   int  dimy = (height+T-1) / T;
-   int  dimx = (width +T-1) / T;
+   int dimy = CEIL_DIV(height, T);
+   int dimx = CEIL_DIV(width, T);
    dim3 block(T, T, 1);
    dim3 grid (dimx, dimy, 1);
+   unsigned int sh_mem_size = T * (T + 1) * sizeof(ElTp);
 
-   //2. execute the kernel
    transpose_tiled_kernel<ElTp,T><<<grid, block, sh_mem_size>>>
                                  (d_in, d_out, height, width);
-   cudaDeviceSynchronize();
 }
 
 static void transpose(void *d_A, void *d_B, int heightA, int widthA)
